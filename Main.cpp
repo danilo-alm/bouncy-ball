b8b2af1ad4b81d2ad8774395df7e65138a3f1de0
Danilo Almeida <danilo.alm@tutanota.com>
2023-04-07 12:44:07 -0300

    implement jumping and add 'moveSideways' function template

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,7 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
-#include <string>
+
+enum Side {left, right};
 
 class BouncyBall
 {
@@ -10,6 +11,7 @@ private:
     sf::Vector2f maxVelocity = sf::Vector2f(20, 20);
     sf::Vector2f velocity = sf::Vector2f(0, 0);
     sf::CircleShape circle;
+    bool isJumping = true;
 
 public:
     BouncyBall(float pos_x, float pos_y, float radius)
@@ -22,18 +24,37 @@ public:
         circle.setPosition(position);
         circle.setFillColor(sf::Color::White);
     }
+    void moveSideways(Side side)
+    {
+        switch (side)
+        {
+            case left:
+                break;
+            case right:
+                break;
+        }
+    }
+    void jump()
+    {
+        if (!isJumping)
+        {
+            velocity.y = -23;
+            isJumping = true;
+        }
+    }
     void updatePhysics(uint32_t& windowWidth, uint32_t& windowHeight)
     {
         // Gravity
         float nextYVelocity = velocity.y + acceleration.y;
         float nextYPosition = position.y + velocity.y;
-        if (position.y < windowHeight - circle.getRadius() && nextYPosition < windowHeight - circle.getRadius())
+        if (nextYPosition < windowHeight - circle.getRadius())
         {
             velocity.y = nextYVelocity;
             position.y = nextYPosition;
         }
         else
         {
+            isJumping = false;
             velocity.y = 0.f;
             position.y = windowHeight - circle.getRadius();
         }
@@ -48,36 +69,43 @@ public:
     {
         return velocity;
     }
+    bool getIsJumping()
+    {
+        return isJumping;
+    }
 };
+
+void setTextUp(sf::Text& text, sf::Font& font, float pos_x, float pos_y)
+{
+    text.setFont(font);
+    text.setCharacterSize(24);
+    text.setFillColor(sf::Color::White);
+    text.setPosition(pos_x, pos_y);
+}
 
 int main()
 {
     uint32_t windowWidth = 1280;
     uint32_t windowHeight = 720;
     sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "Bouncy Ball");
-    window.setFramerateLimit(60);
+    window.setFramerateLimit(75);
 
     BouncyBall bouncyBall(windowWidth / 2, 60, 30);
 
     sf::Font font;
-    std::string file = "fonts/Ubuntu-Regular.ttf";
-    if (!font.loadFromFile(file))
+    if (!font.loadFromFile("fonts/Ubuntu-Regular.ttf"))
     {
-        std::cout << "Could not load " << file << std::endl;
         return 1;
     }
 
     sf::Text xVelocitytext;
-    xVelocitytext.setFont(font);
-    xVelocitytext.setCharacterSize(24);
-    xVelocitytext.setFillColor(sf::Color::White);
-    xVelocitytext.setPosition(10, 10);
+    setTextUp(xVelocitytext, font, 10, 10);
 
     sf::Text yVelocitytext;
-    yVelocitytext.setFont(font);
-    yVelocitytext.setCharacterSize(24);
-    yVelocitytext.setFillColor(sf::Color::White);
-    yVelocitytext.setPosition(10, 40);
+    setTextUp(yVelocitytext, font, 10, 40);
+
+    sf::Text isJumpingText;
+    setTextUp(isJumpingText, font, 10, 70);
 
     while (window.isOpen())
     {
@@ -85,10 +113,15 @@ int main()
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed || sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) window.close();
+
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) bouncyBall.moveSideways(left);
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) bouncyBall.moveSideways(right);
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) bouncyBall.jump();
         }
 
         xVelocitytext.setString("X velocity: " + std::to_string(bouncyBall.getVelocity().x));
         yVelocitytext.setString("Y velocity: " + std::to_string(bouncyBall.getVelocity().y));
+        isJumpingText.setString("Is jumping: " + std::to_string(bouncyBall.getIsJumping()));
 
         window.clear();
 
@@ -97,6 +130,7 @@ int main()
 
         window.draw(xVelocitytext);
         window.draw(yVelocitytext);
+        window.draw(isJumpingText);
 
         window.display();
     }
