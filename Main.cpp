ffe9e23b004f5f855c6a039a9354be951ce18b31
Danilo Almeida <danilo.alm@tutanota.com>
2023-04-09 17:14:21 -0300

    create window

--- Main.cpp
+++ Main.cpp
@@ -2,5 +2,27 @@
 
 int main()
 {
-    
+    sf::RenderWindow window(sf::VideoMode(1280, 720), "Bouncy Ball");
+    window.setFramerateLimit(75);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            switch (event.type)
+            {
+                case sf::Event::Closed:
+                    window.close();
+                    break;
+                
+                case sf::Event::KeyPressed:
+                    if (event.key.code == sf::Keyboard::Escape)
+                        window.close();
+                    // else
+                    //     bouncyBall.HandleInput(event.key.code);
+                    break;
+            }
+        }
+    }
 }
